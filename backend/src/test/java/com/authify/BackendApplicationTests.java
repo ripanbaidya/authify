6af5de06645d017d4f5d1a5997f78b21566d3b59
Ripan Baidya <ripanbaidya7980@gmail.com>
2025-12-17 00:33:49 +0530

    project setup

---
+++ backend/src/test/java/com/authify/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
